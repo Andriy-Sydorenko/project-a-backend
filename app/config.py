36707aaefd97eea82fc8e0e9ac8219c4dd1fd138
Andriy Sydorenko <94242534+Andriy-Sydorenko@users.noreply.github.com>
2025-04-20 23:55:04 +0300

    #BE-11: Add main dependencies and base user model to the project (#2)

---
+++ app/config.py
@@ -0,0 +1,39 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "Demo App of Linear Clone"
+    app_version: str = "0.1.0"
+    admin_email: str = "admin@email.com"
+    database_host: str = "localhost"
+    database_port: int = 5432
+    database_name: str = "db_name"
+    database_user: str = "some_name"
+    database_password: str = "some_password"
+
+    allowed_hosts: list[str] = ["*"]
+
+    # TODO: Make key and iv generate automatically on each server start
+    aes_key: str = "invalid_aes_key"
+    aes_iv: str = "invalid_aes_iv"
+    jwt_secret: str = "invalid_jwt_secret"
+    token_expire_minutes: int = 60 * 24
+
+    @property
+    def db_creds(self) -> str:
+        return f"{self.database_user}:{self.database_password}@{self.database_host}:{self.database_port}/{self.database_name}"
+
+    @property
+    def async_db_url(self) -> str:
+        return f"postgresql+asyncpg://{self.db_creds}"
+
+    @property
+    def sync_db_url(self) -> str:
+        return f"postgresql://{self.db_creds}"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
